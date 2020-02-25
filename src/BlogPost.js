0f23c0bf30bd128fa3d9b61d7cc342cd92176053
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-25 01:01:44 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -15,14 +15,11 @@ export default class BlogPost extends Component {
         Just like moons and like suns,<br/>
         With the certainty of tides,<br/>
         Just like hopes springing high,<br/>
-        Still I'll rise.<br/>
+        Still Ill rise.<br/>
         -Maya Angelou<br/>
-
-        {/* (remove the comment ticks and their surrounding brackets)
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        */}
+        <Comment commentText={comments[0]} />  
       </div>
     );
   }
